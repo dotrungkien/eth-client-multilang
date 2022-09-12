e874b3ae4bd53b8809fd5ba8977055dfe3d68d5c
Do Trung Kien <trungkien.keio@gmail.com>
2022-09-12 09:55:36 +0700

    refactor: update Rust client

--- clients/rust/src/main.rs
+++ clients/rust/src/main.rs
@@ -2,28 +2,33 @@ use ethers::prelude::*;
 use ethers::utils::Anvil;
 use eyre::Result;
 use std::{convert::TryFrom, sync::Arc, time::Duration};
+use dotenv::dotenv;
+use std::env;
 
-abigen!(SimpleStorage, "abis/SimpleStorage.json");
+abigen!(SimpleStorage, "../abis/SimpleStorage.json");
 
 #[tokio::main]
 async fn main() -> Result<()> {
+    dotenv().ok();
     let anvil = Anvil::new().fork("http://127.0.0.1:8545").spawn();
-    let wallet: LocalWallet = anvil.keys()[0].clone().into();
     let provider =
         Provider::<Http>::try_from(anvil.endpoint())?.interval(Duration::from_millis(10u64));
-    let client = Arc::new(SignerMiddleware::new(provider, wallet));
 
-    let contract_address = "0x5FbDB2315678afecb367f032d93F642f64180aa3".parse::<Address>()?;
+    let wallet: LocalWallet = anvil.keys()[0].clone().into();
+    let client = Arc::new(SignerMiddleware::new(provider, wallet.with_chain_id(anvil.chain_id())));
+
+    println!("Contract Address {:?}", env::var("SIMPLE_STORAGE").unwrap());
+    let contract_address = env::var("SIMPLE_STORAGE").unwrap().parse::<Address>()?;
     let simple_storage = SimpleStorage::new(contract_address, Arc::clone(&client));
 
     let mut value = simple_storage.get().call().await?;
 
-    println!("value is {:?}", value);
+    println!("value before is {:?}", value);
 
     simple_storage.set(U256::from(54321)).send().await?.await?;
 
     value = simple_storage.get().call().await?;
 
-    println!("value is {:?}", value);
+    println!("value after is {:?}", value);
     Ok(())
 }
